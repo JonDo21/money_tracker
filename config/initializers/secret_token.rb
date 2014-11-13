469ebeffc7d76a1339520c353db5f3831e3a71d7
kdylan <k.dylan4@gmail.com>
2014-11-13 15:15:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoneyTracker::Application.config.secret_key_base = '6cd8ceb1db1ccc3218f8debbb3d4d13bdeb3b86fd2e79b88a9fb44d60b718670ff0cdb3078499915a6ee7f09ec29f58d2ad3851dacfef31f2d6ad8c8effda3f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MoneyTracker::Application.config.session_store :cookie_store, key: '_money_tracker_session'
