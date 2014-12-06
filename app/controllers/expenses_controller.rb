983d97b1a87ff130f725b8e853407e788071a0c7
kdylan <k.dylan4@gmail.com>
2014-12-06 14:43:00 -0800

    add form for new expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,21 @@
 class ExpensesController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   def create
+  	 @expense = current_user.expenses.build(expense_params)
+    if @expense.save
+      flash[:success] = "Expense created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def expense_params
+      params.require(:expense).permit(:content)
+    end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+  	@expense = current_user.expenses.build if logged_in?
   end
 
   def help
